9ebc90ea46e8bbbfec8c79dc90bc0d2d30afab58
ChihYuChiang <ckccyu@gmail.com>
2018-03-26 18:39:02 -0500

    Add: some func to save to files

--- bin/preprocessing.py
+++ bin/preprocessing.py
@@ -3,10 +3,11 @@ import numpy as np
 import re
 import operator
 import jieba
+import pickle
 from nltk.probability import FreqDist
 
 
-df_review_sp = pd.read_csv('D:\OneDrive\Projects\Independent\Restaurant Review\data\df_review_bj.csv', nrows=100) + 
+df_review_sp = pd.read_csv('D:\OneDrive\Projects\Independent\Restaurant Review\data\df_review_bj.csv', nrows=100)
 
 stopwords = list(set([w for l in open(r'..\ref\stopwords\中文停用词库.txt', encoding='GB2312') for w in l.split()] + [w for l in open(r'..\ref\stopwords\哈工大停用词表.txt', encoding='GBK') for w in l.split()]))
 
@@ -14,11 +15,12 @@ def tokenize_sentence(doc):
     punList = ',.!?:;~，。！？：；～'
     senStart = 0
     sentences = []
-    for i in np.arange(len(doc) - 1):
-        if doc[i] in punList and doc[i + 1] not in punList:
-            sentences.append(doc[senStart : i + 1])
-            senStart = i + 1
-    if senStart < len(doc): sentences.append(doc[senStart: ])
+    if isinstance(doc, str):
+        for i in np.arange(len(doc) - 1):
+            if doc[i] in punList and doc[i + 1] not in punList:
+                sentences.append(doc[senStart : i + 1])
+                senStart = i + 1
+        if senStart < len(doc): sentences.append(doc[senStart: ])
     return sentences
 
 def tokenize_word(sentence):
@@ -39,18 +41,21 @@ class Df_review:
     def __iter__(self):
         for p in self.df:
             if self.curChunk == self.maxChunk: break
-            print('Start processing chunk', p)
+            print('Start processing chunk', self.curChunk)
             self.curChunk += 1
             yield from p.iterrows()
 
 df = Df_review('D:\OneDrive\Projects\Independent\Restaurant Review\data\df_review_bj.csv', chunkSize=50000, maxChunk=None)
 
-#Concat sentences
+#Tokenize and preprocessing by doc
 final = []
 for _, r in df:
     sentences = tokenize_sentence(r.Review)
-    final += [remove_stopword(tokenize_word(s)) for s in sentences]
-print(final)
+    final.append([remove_stopword(tokenize_word(s)) for s in sentences])
+print(final[:10])
+
+with open(r'..\data\tokenized.pickle', 'wb') as f:
+    pickle.dump(final, f)
 
 #Word count
 def g(df):
@@ -60,10 +65,13 @@ def g(df):
             words = remove_stopword(tokenize_word(s))
             for w in words: yield w
 fdist = FreqDist(g(df))
-fdist.B() #Distinct word
-fdist.N() #Total word
+with open(r'..\data\fdist.pickle', 'wb') as f:
+    pickle.dump(fdist, f)
+print('Unique terms:', fdist.B())
+print('Total terms:', fdist.N())
 sorted(fdist.items(), key=operator.itemgetter(1), reverse=True)
 
+fdist = FreqDist([i for i in flatten(final)])
 
 def flatten(l):
     import collections
@@ -73,4 +81,4 @@ def flatten(l):
         else:
             yield el
 
-[i for i in flatten(final[:10])]
+[i for i in flatten(final)]
