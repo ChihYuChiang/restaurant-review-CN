e8cd33ace663ba3575614fcffb24254a81399efc
ChihYuChiang <ckccyu@gmail.com>
2018-09-30 02:09:54 +0800

    Add: pref density contour

--- bin/5_plot/script.js
+++ bin/5_plot/script.js
@@ -1,40 +1,61 @@
-d3.json("person_pref.json").then(data => {
-  // Populate a grid of n×m values where -2 ≤ x ≤ 2 and -2 ≤ y ≤ 1.
-  var n = 240,
-    m = 125,
-    values = data.pref.jian;
-  console.log(values);
+d3.json("person_prefpoint.json").then(data => {
 
-  var svg = d3
-      .select("svg")
-      .attr("stroke", "#000")
-      .attr("stroke-width", 0.5),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+  var values = data.point.jian;
 
-  var thresholds = d3.range(1, 10).map(function(p) {
-    return Math.pow(2, p);
-  });
+  var svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height");
+
+  var color = d3.scaleSequential(d3.interpolateYlGnBu)
+    .domain([0, 0.05]);
+
+  var x = d3.scaleLinear()
+    .domain([-100, 100])
+    .rangeRound([0, width]);
+
+  var y = d3.scaleLinear()
+    .domain([-100, 100])
+    .rangeRound([0, height]);
 
   var contours = d3
-    .contours()
-    .size([n, m])
-    .thresholds(d3.range(1, 10));
+    .contourDensity()
+    .x((d) => x(d[0]))
+    .y((d) => y(d[1]))
+    .size([width, height])
+    .bandwidth(10);
 
-  var color = d3
-    .scaleLog()
-    .domain(d3.extent(thresholds))
-    .interpolate(function() {
-      return d3.interpolateYlGnBu;
-    });
-
-  svg
+  svg.insert("g")
+    .attr("stroke", "#FFFFFF")
+    .attr("stroke-width", 0.5)
+    .attr("stroke-linejoin", "round")
     .selectAll("path")
     .data(contours(values))
     .enter()
     .append("path")
-    .attr("d", d3.geoPath(d3.geoIdentity().scale(width / n)))
-    .attr("fill", function(d) {
-      return color(d.value);
-    });
+    .attr("fill", (d) => color(d.value))
+    .attr("d", d3.geoPath());
+
+  // svg.insert("g")
+  //   .selectAll(".dot")
+  //   .data(data)
+  //   .enter().append("circle")
+  //   .attr("class", "dot")
+  //   .attr("r", 3.5)
+  //   .attr("cx", xMap)
+  //   .attr("cy", yMap)
+  //   .style("fill", function (d) { return color(cValue(d)); })
+  //   .on("mouseover", function (d) {
+  //     tooltip.transition()
+  //       .duration(200)
+  //       .style("opacity", .9);
+  //     tooltip.html(d["Cereal Name"] + "<br/> (" + xValue(d)
+  //       + ", " + yValue(d) + ")")
+  //       .style("left", (d3.event.pageX + 5) + "px")
+  //       .style("top", (d3.event.pageY - 28) + "px");
+  //   })
+  //   .on("mouseout", function (d) {
+  //     tooltip.transition()
+  //       .duration(500)
+  //       .style("opacity", 0);
+  //   });
 });
